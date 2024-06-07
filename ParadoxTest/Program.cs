514a9185b4cbc04c063c75e9164e9faffe2ca503
MedilinkPS <61810200+MedilinkPS@users.noreply.github.com>
2024-06-07 12:04:54 +1000

    add app.config connectionstring for test so it can be modified

--- ParadoxTest/Program.cs
+++ ParadoxTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
@@ -8,46 +9,56 @@ using ParadoxReader;
 
 namespace ParadoxTest
 {
-    class Program
+    internal class Program
     {
+
+
         static void Main(string[] args)
         {
-            var dbPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "data");
+
+            var dbPath = ParadoxTest.Configuration.GetParadoxDataFolderPath("Test");
 
             Console.WriteLine("Test 1: sequential read first 10 records from start");
             Console.WriteLine("==========================================================");
-            var table = new ParadoxTable(dbPath, "zakazky");
-            var recIndex = 1;
-            foreach (var rec in table.Enumerate())
+            using (var table = new ParadoxTable(dbPath, "zakazky"))
             {
-                Console.WriteLine("Record #{0}", recIndex++);
-                for (int i=0; i<table.FieldCount; i++)
+                var recIndex = 1;
+                foreach (var rec in table.Enumerate())
                 {
-                    Console.WriteLine("    {0} = {1}", table.FieldNames[i], rec.DataValues[i]);
+                    Console.WriteLine("Record #{0}", recIndex++);
+                    for (int i = 0; i < table.FieldCount; i++)
+                    {
+                        Console.WriteLine("    {0} = {1}", table.FieldNames[i], rec.DataValues[i]);
+                    }
+                    if (recIndex > 10) break;
                 }
-                if (recIndex > 10) break;
-            }
-            Console.WriteLine("-- press [enter] to continue --");
-            Console.ReadKey();
-            Console.Clear();
+                Console.WriteLine("-- press [enter] to continue --");
+                Console.ReadKey();
+                Console.Clear();
 
-            Console.WriteLine("Test 2: read 10 records by index (key range: 1750 -> 1760)");
-            Console.WriteLine("==========================================================");
+                Console.WriteLine("Test 2: read 10 records by index (key range: 1750 -> 1760)");
+                Console.WriteLine("==========================================================");
 
-            var index = new ParadoxPrimaryKey(table, Path.Combine(dbPath, "zakazky.PX"));
-            var condition =
-                new ParadoxCondition.LogicalAnd(
-                    new ParadoxCondition.Compare(ParadoxCompareOperator.GreaterOrEqual, 1750, 0, 0),
-                    new ParadoxCondition.Compare(ParadoxCompareOperator.LessOrEqual, 1760, 0, 0));
-            var qry = index.Enumerate(condition);
-            var rdr = new ParadoxDataReader(table, qry);
-            recIndex = 1;
-            while (rdr.Read())
-            {
-                Console.WriteLine("Record #{0}", recIndex++);
-                for (int i = 0; i < rdr.FieldCount; i++)
+                using (var index = table.PrimaryKeyIndex)
                 {
-                    Console.WriteLine("    {0} = {1}", rdr.GetName(i), rdr[i]);
+                    if (index != null)
+                    {
+                        var condition =
+                            new ParadoxCondition.LogicalAnd(
+                                new ParadoxCondition.Compare(ParadoxCompareOperator.GreaterOrEqual, 2, 0, 0),
+                                new ParadoxCondition.Compare(ParadoxCompareOperator.LessOrEqual, 2, 0, 0));
+                        var qry = index.Enumerate(condition);
+                        var rdr = new ParadoxDataReader(table, qry);
+                        recIndex = 1;
+                        while (rdr.Read())
+                        {
+                            Console.WriteLine("Record #{0}", recIndex++);
+                            for (int i = 0; i < rdr.FieldCount; i++)
+                            {
+                                Console.WriteLine("    {0} = {1}", rdr.GetName(i), rdr[i]);
+                            }
+                        }
+                    }
                 }
             }
             Console.WriteLine("-- press [enter] to continue --");
